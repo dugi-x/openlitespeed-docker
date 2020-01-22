64f859b1e76312cc95c8cb751446fd4cfee139c7
Dagan Yaakov <dugi2000@gmail.com>
2020-01-22 19:28:09 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+
+VERSION=${1:-1.6.5}
+TAG=${1:-latest}
+
+CODE=$(curl -Is https://openlitespeed.org/packages/openlitespeed-${VERSION}.tgz -w %{http_code} -o /dev/null)
+echo $CODE
+if [ $CODE != "200" ]; then
+  echo -e "packages/openlitespeed-${VERSION} unavailable"
+  exit 1
+fi
+
+docker build --build-arg version=${VERSION} -t swarmpit/install:${TAG} .
